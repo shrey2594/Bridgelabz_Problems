4808b38e5d820853bc08ce6204fe8bf0c5101cc8
shrey2594 <shrey2510@gmail.com>
2022-02-23 15:30:48 +0530

    View fibonacci series till any range

---
+++ FibonacciSeries/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FibonacciSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation.Display();
+        }
+    }
+
+}
