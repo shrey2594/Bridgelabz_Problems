14cadfb967a015e5c69a51d1cfd3f34206fd482f
shrey2594 <shrey2510@gmail.com>
2022-02-23 15:36:39 +0530

    Check wether entered year is leap year or not

---
+++ Leap_year/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PrintUsername
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a year:");
+            string year_s=Console.ReadLine();
+            int year_t=int.Parse(year_s);
+            if (year_t>999)
+            {
+                if (year_t%400==0)
+                {
+                    Console.WriteLine("{0} is a leap year.", year_t);
+                }
+                else if (year_t%100==0)
+                {
+                   Console.WriteLine("{0} is not a leap year.", year_t);
+                }
+                else if (year_t%4==0)
+                {
+                    Console.WriteLine("{0} is a leap year.", year_t);
+                }
+                else { Console.WriteLine("{0} is not a leap year.", year_t); }
+            }
+            else
+            {
+                Console.WriteLine("Enter year above 999 and run again.");
+            }
+        }
+    }
+
+}
