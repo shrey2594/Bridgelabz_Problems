4d9108ca698d79dc339a2b8d6461a9447b855b08
shrey2594 <shrey2510@gmail.com>
2022-02-23 15:33:12 +0530

    calculate harmonic number

---
+++ HarmonicNumber/Harmonic_Number.cs
@@ -0,0 +1,15 @@
+﻿namespace Harmonic_Number
+{
+    public class Harmonic_Number
+    {
+        public static double result(int value)
+        {
+            double sum = 0;
+            for(double i = 1; i <= value; i++)
+            {
+                sum=sum+(double)(1/i);
+            }
+            return sum;
+        }
+    }
+}
---
+++ HarmonicNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Harmonic_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the N value:");
+            int N=Convert.ToInt32(Console.ReadLine());
+            if (N!=0)
+            {
+                double value = Harmonic_Number.result(N);
+                Console.WriteLine("Result is:" + value);
+            }
+            else 
+            {
+                Console.WriteLine("Enter non zero value and run again...");
+            }
+            
+        }
+    }
+
+}
