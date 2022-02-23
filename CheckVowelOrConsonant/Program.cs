b079003282a8ca0315192af48fce534bc2e5379b
shrey2594 <shrey2510@gmail.com>
2022-02-23 15:28:54 +0530

    Check Vowel or Consonant

---
+++ CheckVowelOrConsonant/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CheckVowelOrConsonant
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a letter to check for vowel or consonant:");
+            checkVowel.checkVowelorConsonant();
+        }
+    }
+
+}
---
+++ CheckVowelOrConsonant/checkVowel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckVowelOrConsonant
+{
+    public class checkVowel
+    {
+        public static void checkVowelorConsonant()
+        {
+            char ch=Convert.ToChar(Console.ReadLine());
+            if(ch =='a'|| ch=='e'|| ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+            {
+                Console.WriteLine(ch + " is a vowel.");
+            }
+            else if((ch >='a' && ch <='z')||(ch>='A' && ch<='Z'))
+            {
+                Console.WriteLine(ch + " is a Consonant.");
+            }
+            else
+            {
+                Console.WriteLine("Enter only an alphabet.");
+            }
+        }
+    }
+}
