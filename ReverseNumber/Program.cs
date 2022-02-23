236e1b3cc0128859a9171f32ee6470376fd1883f
shrey2594 <shrey2510@gmail.com>
2022-02-23 15:58:39 +0530

    Reverse the number

---
+++ ReverseNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ReverseNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Reverse.ReverseTheNumber();
+        }
+    }
+
+}
---
+++ ReverseNumber/Reverse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseNumber
+{
+    public class Reverse
+    {
+        public static void ReverseTheNumber()
+        {
+            int n, reverse = 0, rem;
+            Console.WriteLine("Enter a number");
+            n=Convert.ToInt32(Console.ReadLine());
+            while(n!=0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n /= 10;
+            }
+            Console.WriteLine("Reversed number is:"+reverse);
+       
+        }
+    }
+}
