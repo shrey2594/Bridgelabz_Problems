0cfaaf1ab69a09b1ab5a7a625ee4d970928de141
shrey2594 <shrey2510@gmail.com>
2022-02-23 15:59:14 +0530

    swap number

---
+++ swapnumber/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace swapnumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Swapping Problem");
+            swappingnumbers.SwapTwoNumbers();
+        }
+    }
+
+}
---
+++ swapnumber/swappingnumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swapnumber
+{
+    public class swappingnumbers
+    {
+        public static void SwapTwoNumbers()
+        {
+            int a = 4;
+            int b = 5;
+            Console.WriteLine("Number a before swapping:" + a);
+            Console.WriteLine("Number b before swapping:" + b);
+            a = a * b;
+            b = a / b;
+            a = a / b;
+            Console.WriteLine("Number a after swapping:" + a);
+            Console.WriteLine("Number b after swapping:" + b);
+        }
+    }
+}
