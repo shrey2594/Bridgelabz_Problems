5a6b0fefa3613f1d01139d4641b085cc21b89a53
shrey2594 <shrey2510@gmail.com>
2022-02-23 15:54:47 +0530

    factorize the number into factors

---
+++ primefactor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace primefactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number to check prime factor :");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(N);
+            primefactor.factorization(N);
+        }
+    }
+
+}
---
+++ primefactor/primefactor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primefactor
+{
+    public class primefactor
+    {
+        public static void factorization(int number)
+        {
+            int j = 1;
+            int k = 0;
+            int [] prime = new int[number];
+            while(j!=number)
+            {
+                if (number % j == 0)
+                {
+                    prime[k] = j;
+                    k++;
+                }
+                j++;
+            }
+            Console.WriteLine("Prime Factors of "+number+" are:"+prime);
+        }
+    }
+}
