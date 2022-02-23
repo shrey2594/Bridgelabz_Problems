93886585c064d133cc2924fd5d2b5219ad54978b
shrey2594 <shrey2510@gmail.com>
2022-02-23 15:46:47 +0530

    Calculate power of 2

---
+++ power2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace power2    
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a value between 0 and 31:");
+            int pow=Convert.ToInt32(Console.ReadLine());
+            if (pow>=0 && pow<31)
+            {
+                power2.power_calculator_2(pow);
+            } 
+            else
+            {
+                Console.WriteLine("Run again with a value between 0 and 31.");
+            }
+        }
+    }
+
+}
---
+++ power2/power2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace power2
+{
+    public class power2
+    {
+        public static void power_calculator_2(int value)
+        {
+            for (int i = 0; i <= value; i++)
+            {
+                Console.WriteLine("2*"+i+"="+Math.Pow(2,i));
+            }
+        }
+    }
+}
