ce80795597e3a6a630ef4c72592625544adde64c
shrey2594 <shrey2510@gmail.com>
2022-02-23 15:58:03 +0530

    Quotient and remainder finder

---
+++ QuotientRemainder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace QuotientRemainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a dividend");
+            int dividend=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a divisor");
+            int divisor=Convert.ToInt32(Console.ReadLine());
+            Quotient.print(dividend, divisor);
+        }
+    }
+
+}
---
+++ QuotientRemainder/Quotient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuotientRemainder
+{
+    public class Quotient
+    {
+        public static void print(int dividend,int divisor)
+        {
+            Console.WriteLine("The quotient is:" + (dividend / divisor));
+            Console.WriteLine("The remainder is:"+(dividend % divisor)); 
+        }
+    }
+}
