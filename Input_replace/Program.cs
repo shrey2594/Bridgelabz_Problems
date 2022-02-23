cf84a81a99320cb8fdd33eef7197c47b17aa8721
shrey2594 <shrey2510@gmail.com>
2022-02-23 15:34:59 +0530

    read Input

---
+++ Input_replace/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PrintUsername
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Username:");
+            string UserName = Console.ReadLine();
+            if (UserName.Length>=3)
+            {
+                Console.WriteLine("Hi, username:"+UserName+" ,have a good day.");
+            }
+            else
+            {
+                Console.WriteLine("Enter username again , with more than 3 characters.");
+            }
+        }
+    }
+
+}
+
