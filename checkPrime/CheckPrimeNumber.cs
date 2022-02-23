f091be02612d8a1ad82c40dfdae08c855f045cb6
shrey2594 <shrey2510@gmail.com>
2022-02-23 15:26:32 +0530

    Check prime Number or not

---
+++ checkPrime/CheckPrimeNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace checkPrime
+{
+    public class CheckPrimeNumber
+    {
+        public static void check(int number)
+        {
+            int count = 0;
+            int mirror = number;
+            while (mirror > 0)
+            {
+                if ( number % mirror == 0 )
+                {
+                    count++;
+                }
+                mirror--;
+            }
+            if(count>2)
+            {
+                Console.WriteLine("The number is not prime");
+            }
+            else
+            {
+                Console.WriteLine("The number is prime.");
+            }
+        }
+    }
+}
---
+++ checkPrime/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace checkPrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to check if it is prime or not:");
+            int number=Convert.ToInt32(Console.ReadLine());
+            CheckPrimeNumber.check(number);
+        }
+    }
+
+}
